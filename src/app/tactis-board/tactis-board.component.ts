7f2b032dd34f3834fc26d29ab2e896712fe3568d
Pedro Pereira <1211131@isep.ipp.pt>
2025-02-06 22:21:12 +0000

    add styling and structure for tactis board component

--- src/app/tactis-board/tactis-board.component.ts
+++ src/app/tactis-board/tactis-board.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tactis-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tactis-board.component.html',
   styleUrl: './tactis-board.component.css'
 })
 export class TactisBoardComponent {
-
+  
 }
